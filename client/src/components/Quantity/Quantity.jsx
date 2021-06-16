0472d6a94d35d7b0fff3037e227b9d0568c2505f
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-06-16 21:25:32 +0200

    add product actions

--- client/src/components/Quantity/Quantity.jsx
+++ client/src/components/Quantity/Quantity.jsx
@@ -1,8 +1,26 @@
 import { Component } from 'react';
+import { Image } from 'react-bootstrap';
+import classes from './Quantity.module.scss';
 
 class Quantity extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<div>
+				<div className={classes.counter}>
+					<button className={classes.btn}>
+						<Image src="/images/minus.svg" fluid />
+					</button>
+					<span>1</span>
+					<button className={classes.btn}>
+						<Image src="/images/plus.svg" fluid />
+					</button>
+				</div>
+				<div className="actions mt-4">
+					<button className={classes.add_to_cart}>Add To Cart</button>
+					<button className={classes.pick__up}>Pickup From Store</button>
+				</div>
+			</div>
+		);
 	}
 }
 
