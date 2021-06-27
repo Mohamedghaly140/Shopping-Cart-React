0ca932514f9d91f76fd0efc71723d76da7da3221
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-06-27 15:28:14 +0200

    responsive products item for 320px

--- client/src/components/UI/PickupInfo/PickupInfo.jsx
+++ client/src/components/UI/PickupInfo/PickupInfo.jsx
@@ -1,39 +1,39 @@
-import { Component, Fragment } from 'react';
-import classes from './PickupInfo.module.scss';
+import { Component, Fragment } from "react";
+import classes from "./PickupInfo.module.scss";
 
 export default class PickupInfo extends Component {
-	render() {
-		const { pickup_place, pickup_info } = this.props;
+  render() {
+    const { pickup_place, pickup_info } = this.props;
 
-		return (
-			<Fragment>
-				{pickup_place && (
-					<div className="d-flex align-items-center justify-content-center">
-						<span className={classes.pickup}>Pickup From: </span>
-						<span className={classes.pickup__value}>{pickup_place}</span>
-					</div>
-				)}
-				{pickup_info && (
-					<div className="d-flex align-items-center justify-content-between px-2">
-						<div>
-							<span className={classes.pickup}>From: </span>
-							<span className={classes.pickup__value}>{pickup_info.from}</span>
-						</div>
-						<div>
-							<span className={classes.pickup}>To: </span>
-							<span className={classes.pickup__value}>
-								{pickup_info.countery}
-							</span>
-						</div>
-						<div>
-							<span className={classes.pickup}>To: </span>
-							<span className={classes.pickup__value}>
-								{pickup_info.delivery_in}
-							</span>
-						</div>
-					</div>
-				)}
-			</Fragment>
-		);
-	}
+    return (
+      <Fragment>
+        {pickup_place && (
+          <div className="d-flex align-items-center justify-content-center pickup__info">
+            <span className={classes.pickup}>Pickup From: </span>
+            <span className={classes.pickup__value}>{pickup_place}</span>
+          </div>
+        )}
+        {pickup_info && (
+          <div className="d-flex align-items-center justify-content-between px-2 pickup__info">
+            <div>
+              <span className={classes.pickup}>From: </span>
+              <span className={classes.pickup__value}>{pickup_info.from}</span>
+            </div>
+            <div>
+              <span className={classes.pickup}>To: </span>
+              <span className={classes.pickup__value}>
+                {pickup_info.countery}
+              </span>
+            </div>
+            <div>
+              <span className={classes.pickup}>To: </span>
+              <span className={classes.pickup__value}>
+                {pickup_info.delivery_in}
+              </span>
+            </div>
+          </div>
+        )}
+      </Fragment>
+    );
+  }
 }
--- client/src/components/UI/Price/Price.jsx
+++ client/src/components/UI/Price/Price.jsx
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import { Image } from 'react-bootstrap';
-import classes from './Price.module.scss';
+import React, { Component } from "react";
+import { Image } from "react-bootstrap";
+import classes from "./Price.module.scss";
 
 export default class Price extends Component {
-	render() {
-		const { discount, price, discount_percent, discount_price, inline } =
-			this.props;
+  render() {
+    const { discount, price, discount_percent, discount_price, inline } =
+      this.props;
 
-		return (
-			<div className={classes.price__container}>
-				<div
-					className={`${
-						inline && 'd-flex align-items-center justify-content-center'
-					}`}
-				>
-					<div className={classes.price}>{price} L.E</div>
-					{discount && (
-						<div className={classes.discount__price}>
-							<span className={`${inline && 'mx-3'}`}>
-								{discount_price} L.E
-							</span>
-							<div className={classes.discount__percent}>
-								{discount_percent}%
-							</div>
-						</div>
-					)}
-				</div>
-				{!inline && (
-					<div>
-						<Image fluid src="images/adidas.svg" />
-					</div>
-				)}
-			</div>
-		);
-	}
+    return (
+      <div className={classes.price__container}>
+        <div
+          className={`${
+            inline && "d-flex align-items-center justify-content-center"
+          }`}
+        >
+          <div className={classes.price}>{price} L.E</div>
+          {discount && (
+            <div className={classes.discount__price}>
+              <span className={`${inline && "mx-3"}`}>
+                {discount_price} L.E
+              </span>
+              <div className={classes.discount__percent}>
+                {discount_percent}% Off
+              </div>
+            </div>
+          )}
+        </div>
+        {!inline && (
+          <div className={classes.brand__container}>
+            <Image fluid src="images/adidas.svg" />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
