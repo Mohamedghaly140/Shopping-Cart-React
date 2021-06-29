5b646ce49db6ae852c62076b5bdcf06a57e8a927
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-06-29 16:08:38 +0200

    add responsive for footer

--- client/src/components/Footer/Copyright.jsx
+++ client/src/components/Footer/Copyright.jsx
@@ -1,23 +1,24 @@
-import { Component } from 'react';
-import { Image } from 'react-bootstrap';
+import { Component } from "react";
+import { Image } from "react-bootstrap";
+import classes from "./Copyright.module.scss";
 
 class Copyright extends Component {
-	render() {
-		return (
-			<div className="d-flex align-items-center justify-content-between pt-4">
-				<p className="mb-0">© 2021 yeshtery, all rights reserved.</p>
-				<div>
-					<Image src="images/cash.png" fluid />
-					<Image className="mx-3" src="images/visa.png" fluid />
-					<Image src="images/matercard.png" fluid />
-				</div>
-				<p className="mb-0">
-					<span className="d-inline-block">Powered By</span>{' '}
-					<Image src="images/nas_nav.svg" fluid />
-				</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className={classes.copyright}>
+        <p className="mb-2">© 2021 yeshtery, all rights reserved.</p>
+        <div className={classes.payments__methods}>
+          <Image src="/images/cash.png" fluid />
+          <Image className="mx-3" src="/images/visa.png" fluid />
+          <Image src="/images/matercard.png" fluid />
+        </div>
+        <p>
+          <span className="d-inline-block">Powered By</span>{" "}
+          <Image src="/images/nas_nav.svg" fluid />
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Copyright;
--- client/src/components/UI/Subscribe/Subscribe.jsx
+++ client/src/components/UI/Subscribe/Subscribe.jsx
@@ -1,18 +1,22 @@
-import { Component } from 'react';
-import { Image } from 'react-bootstrap';
-import './Subscribe.scss';
+import { Component } from "react";
+import { Image } from "react-bootstrap";
+import classes from "./Subscribe.module.scss";
 
 class Subscribe extends Component {
-	render() {
-		return (
-			<div className="subscribe">
-				<input type="email" placeholder="Enter Your Mail" />
-				<button className="send">
-					Subscribe <Image className="ml-1" src="images/send.svg" fluid />
-				</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className={classes.subscribe}>
+        <input
+          className={classes.input}
+          type="email"
+          placeholder="Enter Your Mail"
+        />
+        <button className={classes.send}>
+          Subscribe <Image className="ml-1" src="/images/send.svg" fluid />
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Subscribe;
