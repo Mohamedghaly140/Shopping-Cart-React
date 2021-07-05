2f2f01e5a1c092fafadef6cdf4546cecdb95c954
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-07-05 16:41:18 +0200

    remove bootstrap class

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import CategoryItem from "../UI/CategoryItem/CategoryItem";
+import classes from "./Categories.module.scss";
 
 import categoriesData from "../../services/all-categories.json";
 
 class Categories extends Component {
   render() {
     return (
-      <div className="row">
+      <div className={classes.categories__container}>
         {categoriesData.map(category => (
           <div
             key={category.id}
-            className={`col-6 col-md-4 col-lg-4 ${
-              category.hidden && "d-sm-none"
+            className={`${category.hidden ? classes.category__hidden : ""} ${
+              classes.category__container
             }`}
           >
             {category.items.map(item => (
@@ -32,3 +33,60 @@ class Categories extends Component {
 }
 
 export default Categories;
+
+/* 
+{
+    "id": 4,
+    "hidden": true,
+    "items": [
+      {
+        "id": 1,
+        "title": "Cosmetics",
+        "description": "Beautiful. Colorful. You.",
+        "imageUrl": "/images/categories/cosmetics@2x.png",
+        "lg": false,
+        "sm": true
+      },
+      {
+        "id": 2,
+        "title": "Electronics",
+        "description": "Find latest electronic devices.",
+        "imageUrl": "/images/categories/electronics@2x.png",
+        "lg": true,
+        "sm": false
+      },
+      {
+        "id": 3,
+        "title": "Cosmetics",
+        "description": "Beautiful. Colorful. You.",
+        "imageUrl": "/images/categories/cosmetics@2x.png",
+        "lg": false,
+        "sm": true
+      },
+      {
+        "id": 4,
+        "title": "Electronics",
+        "description": "Find latest electronic devices.",
+        "imageUrl": "/images/categories/electronics@2x.png",
+        "lg": true,
+        "sm": false
+      },
+      {
+        "id": 5,
+        "title": "Cosmetics",
+        "description": "Beautiful. Colorful. You.",
+        "imageUrl": "/images/categories/cosmetics@2x.png",
+        "lg": false,
+        "sm": true
+      },
+      {
+        "id": 6,
+        "title": "Electronics",
+        "description": "Find latest electronic devices.",
+        "imageUrl": "/images/categories/electronics@2x.png",
+        "lg": true,
+        "sm": false
+      }
+    ]
+  }
+*/
