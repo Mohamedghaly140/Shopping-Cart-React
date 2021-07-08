ea64e9a1b2ecaa047b8b2e1724d93188c2ba9039
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-07-08 16:34:43 +0200

    optmize all categories list

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { debounce } from "throttle-debounce";
 import CategoryItem from "../UI/CategoryItem/CategoryItem";
 import classes from "./Categories.module.scss";
@@ -33,68 +33,40 @@ class Categories extends Component {
     }
   };
 
-  render() {
+  clacLgItem(outer, inner) {
     const { small } = this.state;
 
-    const rows = new Array(Math.ceil(categoriesData.length) / 6);
+    if (small) {
+      return outer % 2 === inner % 2;
+    } else {
+      return (outer % 3) % 2 === inner % 2;
+    }
+  }
+
+  render() {
+    const rows = new Array(Math.ceil(categoriesData.length) / 2);
 
     for (let i = 0; i < rows.length; i++) {
-      rows[i] = { items: categoriesData.slice(i * 6, i * 6 + 6) };
+      rows[i] = { items: categoriesData.slice(i * 2, i * 2 + 2) };
     }
 
-    const categoriesListLarge = rows.map((item, index) => (
-      <Fragment key={index}>
-        <div className={classes.category__item}>
-          {item.items.slice(0, 2).map((category, i) => (
-            <CategoryItem
-              key={category.id}
-              lg={i % 2 === 0}
-              title={category.title}
-              imageUrl={category.imageUrl}
-              description={category.description}
-            />
-          ))}
-        </div>
-        <div className={classes.category__item}>
-          {item.items.slice(2, 4).map((category, i) => (
-            <CategoryItem
-              key={category.id}
-              lg={i % 2}
-              title={category.title}
-              imageUrl={category.imageUrl}
-              description={category.description}
-            />
-          ))}
-        </div>
-        <div className={classes.category__item}>
-          {item.items.slice(4, 6).map((category, i) => (
-            <CategoryItem
-              key={category.id}
-              lg={i % 2 === 0}
-              title={category.title}
-              imageUrl={category.imageUrl}
-              description={category.description}
-            />
-          ))}
-        </div>
-      </Fragment>
+    const categoriesList = rows.map((item, index) => (
+      <div key={index} className={classes.category__item}>
+        {item.items.map((category, i) => (
+          <CategoryItem
+            key={category.id}
+            lg={this.clacLgItem(index, i)}
+            title={category.title}
+            imageUrl={category.imageUrl}
+            description={category.description}
+          />
+        ))}
+      </div>
     ));
-
-    const categoriesListSmall = categoriesData.map((category, i) => {
-      return (
-        <CategoryItem
-          key={category.id}
-          lg={i % 2 === 0}
-          title={category.title}
-          imageUrl={category.imageUrl}
-          description={category.description}
-        />
-      );
-    });
 
     return (
       <section className={classes.categories__container}>
-        {small ? categoriesListSmall : categoriesListLarge}
+        {categoriesList}
       </section>
     );
   }
