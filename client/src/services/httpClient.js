c0f9f3e91e529300fa376338b498e0a786a15dfb
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-07-12 11:47:52 +0200

    add brand images

--- client/src/services/httpClient.js
+++ client/src/services/httpClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: `https://react-cart-node.herokuapp.com`,
-  baseURL: `http://localhost:5000`,
+  baseURL: `https://react-cart-node.herokuapp.com`,
+  // baseURL: `http://localhost:5000`,
 });
 
 instance.defaults.headers.post["Content-Type"] = "application/json";
