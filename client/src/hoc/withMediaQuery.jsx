5d34fefdf4296be12044995e144dcb1386fb93d0
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-08-05 14:31:28 +0200

    change withMediaQuery to PureComponent

--- client/src/hoc/withMediaQuery.jsx
+++ client/src/hoc/withMediaQuery.jsx
@@ -1,10 +1,18 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
+
+function currentWidth() {
+  return {
+    isMobile: window.innerWidth <= 576,
+    isMobileSm: window.innerWidth <= 320,
+    isTablet: window.innerWidth >= 768 && window.innerWidth <= 992,
+    isTabletLg: window.innerWidth >= 992 && window.innerWidth <= 1200,
+    isDesktop: window.innerWidth >= 1200,
+  };
+}
 
 export const withMediaQuery = WrappedComponent => {
-  return class WithMediaQuery extends Component {
-    state = {
-      viewportWidth: window.innerWidth,
-    };
+  return class WithMediaQuery extends PureComponent {
+    state = currentWidth();
 
     componentDidMount() {
       window.addEventListener("resize", this.updateWindowDimensions);
@@ -15,19 +23,12 @@ export const withMediaQuery = WrappedComponent => {
     }
 
     updateWindowDimensions = () => {
-      this.setState({ viewportWidth: window.innerWidth });
+      this.setState(currentWidth());
     };
 
     render() {
-      const { viewportWidth } = this.state;
-
-      const isMobile = Boolean(viewportWidth <= 576);
-      const isMobileSm = Boolean(viewportWidth <= 320);
-      const isTablet = Boolean(viewportWidth >= 768 && viewportWidth <= 991.98);
-      const isTabletLg = Boolean(
-        viewportWidth >= 992 && viewportWidth <= 1199.98
-      );
-      const isDesktop = Boolean(viewportWidth >= 1200);
+      const { isMobile, isMobileSm, isTablet, isTabletLg, isDesktop } =
+        this.state;
 
       return (
         <WrappedComponent
