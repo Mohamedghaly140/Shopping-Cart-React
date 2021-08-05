85f57322f97313c712a8ea84ca1eb31496bc50fc
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-08-05 11:35:49 +0200

    convert backdrop to function component

--- client/src/components/Backdrop/Backdrop.jsx
+++ client/src/components/Backdrop/Backdrop.jsx
@@ -1,35 +1,17 @@
-import { Component, Fragment } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import classes from "./Backdrop.module.scss";
 
-class BackDrop extends Component {
-  render() {
-    const { open, onToggle } = this.props;
-
-    return (
-      <div
-        onClick={onToggle}
-        className={`${classes.backdrop} ${open ? classes.open : classes.close}`}
-      >
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-class Backdrop extends Component {
-  render() {
-    const { open, onToggle } = this.props;
-
-    return (
-      <Fragment>
-        {ReactDOM.createPortal(
-          <BackDrop open={open} onToggle={onToggle} />,
-          document.getElementById("backdrop")
-        )}
-      </Fragment>
-    );
-  }
-}
+const Backdrop = ({ open, onToggle, children }) => {
+  return ReactDOM.createPortal(
+    <div
+      onClick={onToggle}
+      className={`${classes.backdrop} ${open ? classes.open : classes.close}`}
+    >
+      {children}
+    </div>,
+    document.getElementById("backdrop")
+  );
+};
 
 export default Backdrop;
